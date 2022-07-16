aa176c7942a4f36b704e41577832c3835e442b53
ArjobanSingh <arjobansingh1998@gmail.com>
2022-07-17 00:35:55 +0530

    added tags tables and many to many relationship with indexes for planetscale

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,15 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-// eslint-disable-next-line import/no-mutable-exports
-let prisma;
+const prisma = global.prisma || new PrismaClient({ log: ["query"] });
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
